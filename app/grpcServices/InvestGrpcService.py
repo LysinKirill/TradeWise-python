ec1e61db21ba329eb3150d79b8d9ffca47ee869e
Kirill Lysin <lysinkirill@gmail.com>
2025-04-10 04:07:02 +0300

    [BACKEND-22]: implement invest grpc service

---
+++ app/grpcServices/InvestGrpcService.py
@@ -0,0 +1,40 @@
+from app.debug.ExceptionLogger import exception_logging
+from app.domain.models.invest import InstrumentModel
+from app.domain.services.IInvestService import IInvestService
+from app.infrastructure.JwtAuthorizationDecorator import jwt_authorization
+from app.infrastructure.RequestResponseLogging import request_response_logging
+from app.proto import invest_pb2, invest_pb2_grpc
+import app.domain.services.IClaimValuesService as IClaimValuesService
+
+class InvestGrpcService(invest_pb2_grpc.InvestServiceServicer):
+    def __init__(
+            self,
+            invest_service: IInvestService,
+            claim_values_service: IClaimValuesService
+    ):
+        self.invest_service = invest_service
+        self.claim_values_service = claim_values_service
+
+    @exception_logging
+    @request_response_logging()
+    @jwt_authorization
+    def GetSupportedInstruments(self, request, context):
+        response = self.user_service.get_accounts()
+
+        return invest_pb2.GetSupportedInstrumentsResponse(instruments=
+        [UserGrpcService.__get_instrument_from_model(instrument) for instrument in response.instruments])
+
+
+    @staticmethod
+    def __get_instrument_from_model(instrument: InstrumentModel.InstrumentModel):
+        return invest_pb2.InstrumentInfo(
+            id=instrument.id,
+            figi=instrument.figi,
+            name=instrument.name,
+            lot=instrument.lot,
+            currency=instrument.currency,
+            sector=instrument.sector,
+            buy_available=instrument.buy_available,
+            sell_available=instrument.sell_available,
+            risk_level=invest_pb2.RiskLevel(instrument.risk_level),
+        )
