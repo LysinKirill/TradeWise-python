5ec61ad6aee32ca523e97c3c76143418d5c539b9
Kirill Lysin <lysinkirill@gmail.com>
2025-05-22 23:23:28 +0300

    [master]: fix: add missing external preprocessor directive

--- externalClients/TInvestApi/proto/orders_pb2_grpc.py
+++ externalClients/TInvestApi/proto/orders_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import orders_pb2 as orders__pb2
+from externalClients.TInvestApi.proto import orders_pb2 as orders__pb2
 
 GRPC_GENERATED_VERSION = '1.69.0'
 GRPC_VERSION = grpc.__version__
